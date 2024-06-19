b552bda78e759133eb085efeca19818f8af6a0aa
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-06-18 23:22:26 -0300

    feat: request password update use-case

---
+++ src/core/use-cases/request-password-update.spec.ts
@@ -0,0 +1,72 @@
+// Services
+import { makeUser } from "@/test/factories/make-user";
+import { MockedMailer } from "@/test/mail/mocked-mailer";
+import { MockedHasher } from "@/test/cryptography/mocked-hasher";
+
+// Use-cases
+import { RequestPasswordUpdateUseCase } from "@/core/use-cases/request-password-update";
+
+// Repositories
+import { InMemoryUsersRepository } from "@/test/repositories/in-memory-users-repository";
+
+// Test
+import { waitFor } from "@/test/utils/wait-for";
+import { MockInstance } from "vitest";
+
+// Errors
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found";
+
+let repositories: {
+  users: InMemoryUsersRepository;
+};
+
+let mocks: {
+  mailer: MockedMailer;
+  hasher: MockedHasher;
+};
+
+let sut: RequestPasswordUpdateUseCase;
+
+let spy: MockInstance;
+
+describe("request password update", () => {
+  beforeEach(() => {
+    repositories = {
+      users: new InMemoryUsersRepository(),
+    };
+
+    mocks = {
+      mailer: new MockedMailer(),
+      hasher: new MockedHasher(),
+    };
+
+    sut = new RequestPasswordUpdateUseCase(
+      repositories.users,
+      mocks.mailer,
+      mocks.hasher
+    );
+
+    spy = vi.spyOn(mocks.mailer, "send");
+  });
+
+  it("should be able to request a password update", async () => {
+    const user = makeUser();
+    await repositories.users.create(user);
+
+    await sut.execute({
+      email: user.email,
+    });
+
+    await waitFor(() => {
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  it("nonexisting user should not be able to request a password update", async () => {
+    await expect(
+      sut.execute({
+        email: "eduardo@email.com",
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
