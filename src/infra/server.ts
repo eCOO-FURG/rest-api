523be4ed4fa9a7094d08a69cad1e4d89f3cca9ee
timoteostifft <timoteostifft@gmail.com>
2024-06-25 18:36:07 -0300

    feat: default error handler

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,12 +1,33 @@
 // Libs
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 // Routes
 import { router } from "@/infra/http/routes";
+import { ZodError } from "zod";
+// import { ZodError } from "zod";
 
 const server = express();
 
 server.use(express.json());
 server.use(router);
 
-server.listen(3333, () => console.log("🚀 HTTP server running"));
+server.use(
+  (error: unknown, _: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ZodError) {
+      const issues = error.issues.map((issue) => ({
+        field: issue.path[0],
+        message: issue.message.toLowerCase(),
+      }));
+
+      return response
+        .status(400)
+        .send({ message: "Erro de validação.", issues });
+    }
+
+    return response
+      .status(500)
+      .send({ message: "💥 Ocorreu um erro interno." });
+  }
+);
+
+server.listen(3333, () => console.log("🚀 Servidor HTTP rodando!"));
