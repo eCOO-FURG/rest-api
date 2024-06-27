362b3227c03e3da6eec475889cbc8815f4150d28
timoteostifft <timoteostifft@gmail.com>
2024-06-26 23:01:36 -0300

    feat: on registered event to infra

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -9,16 +9,24 @@ import { MockedEncrypter } from "@/test/cryptography/mocked-encrypter";
 
 // Use-cases
 import { RegisterUseCase } from "@/core/use-cases/register";
+import { OnRegisteredEvent } from "@/core/events/on-registered";
+import { MockedMailer } from "@/test/mail/mocked-mailer";
 
 const container = createContainer();
 
 container.register({
+  // repositories
   usersRepository: asClass(InMemoryUsersRepository).singleton(),
+  // services
   encrypter: asClass(MockedEncrypter).singleton(),
+  mailer: asClass(MockedMailer),
+  // use-cases
   registerUsecase: asFunction(
     ({ usersRepository, encrypter }) =>
       new RegisterUseCase(usersRepository, encrypter)
   ),
+  // events
+  onRegisteredEvent: asFunction(({ mailer }) => new OnRegisteredEvent(mailer)),
 });
 
 export default container;
