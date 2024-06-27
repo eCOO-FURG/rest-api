9584d05e0d4ad0b73af9e2ea3693b36fa951a9c2
timoteostifft <timoteostifft@gmail.com>
2024-06-26 23:53:47 -0300

    refactor: use token in verify use-case

--- src/core/use-cases/verify-user.spec.ts
+++ src/core/use-cases/verify-user.spec.ts
@@ -5,55 +5,63 @@ import { InMemoryUsersRepository } from "@/test/repositories/in-memory-users-rep
 import { VerifyUserUsecase } from "./verify-user";
 
 // Services
+import { MockedHasher } from "@/test/cryptography/mocked-hasher";
 import { makeUser } from "@/test/factories/make-user";
 
 // Errors
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found";
 import { UserAlreadyVerified } from "@/core/errors/user-already-verified";
+import { WrongCredentialsError } from "../errors/wrong-credentials";
 
 let usersRepository: InMemoryUsersRepository;
+let hasher: MockedHasher;
 
-let sut: VerifyUserUsecase
+let sut: VerifyUserUsecase;
 
 describe("Verify user", () => {
   beforeEach(() => {
-    usersRepository = new InMemoryUsersRepository()
+    usersRepository = new InMemoryUsersRepository();
+    hasher = new MockedHasher();
 
-    sut = new VerifyUserUsecase(
-      usersRepository
-    )
-  })
+    sut = new VerifyUserUsecase(usersRepository, hasher);
+  });
 
   it("should be able verify a user", async () => {
-    const user = makeUser()
-    await usersRepository.create(user)
+    const user = makeUser();
+    await usersRepository.create(user);
 
-    await sut.execute({
-      user_id: user.id.value
-    })
+    const token = await hasher.hash({ user_id: user.id.value });
+
+    await sut.execute({ token });
 
     expect(user.verified_at).not.toBeNull();
     expect(user.verified_at).toBeInstanceOf(Date);
-  })
+  });
+
+  it("should not be able verify with an invalid token", async () => {
+    await expect(() => sut.execute({ token: "123" })).rejects.toBeInstanceOf(
+      WrongCredentialsError
+    );
+  });
 
   it("should not be able verify a user if the user does not exist", async () => {
-    await expect(() => 
-      sut.execute({
-        user_id: '1234',
-      })
-    ).rejects.toBeInstanceOf(ResourceNotFoundError)
-  })
+    const token = await hasher.hash({ user_id: "123" });
+
+    await expect(() => sut.execute({ token })).rejects.toBeInstanceOf(
+      ResourceNotFoundError
+    );
+  });
 
   it("should not be able to be verified if the user is already verified", async () => {
     const user = makeUser({
-      verified_at: new Date()
-    })
-    await usersRepository.create(user)
+      verified_at: new Date(),
+    });
+    await usersRepository.create(user);
 
-    await expect(() => 
-      sut.execute({
-        user_id: user.id.value,
-      })
-    ).rejects.toBeInstanceOf(UserAlreadyVerified)
-  })
-})
+    const token = await hasher.hash({ user_id: user.id.value });
+
+    await expect(() => sut.execute({ token })).rejects.toBeInstanceOf(
+      UserAlreadyVerified
+    );
+  });
+});
