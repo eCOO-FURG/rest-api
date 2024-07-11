a55963714425270f00a5de99879c2ef96245a7c1
timoteostifft <timoteostifft@gmail.com>
2024-07-11 01:42:12 -0300

    feat: prisma repositories on middlewares

--- src/infra/database/repositories/prisma-sessions-repository.ts
+++ src/infra/database/repositories/prisma-sessions-repository.ts
@@ -8,6 +8,13 @@ import { SessionsRepository } from "@/core/repositories/sessions-repository";
 import { prisma } from "@/infra/database/prisma-service";
 import { PrismaSessionMapper } from "@/infra/database/mappers/prisma-session-mapper";
 
+export interface PrismaSessionsRepositorySearchRequest {
+  user_id: string;
+  ip: string;
+  agent: string;
+  since: Date;
+}
+
 export class PrismaSessionsRepository implements SessionsRepository {
   async create(session: Session): Promise<void> {
     const data = PrismaSessionMapper.toPrisma(session);
@@ -15,5 +22,27 @@ export class PrismaSessionsRepository implements SessionsRepository {
     await prisma.session.create({
       data,
     });
+  }
+
+  async search({
+    user_id,
+    ip,
+    agent,
+    since,
+  }: PrismaSessionsRepositorySearchRequest) {
+    const session = await prisma.session.findFirst({
+      where: {
+        user_id,
+        ip,
+        agent,
+        created_at: {
+          gte: since,
+        },
+      },
+    });
+
+    if (!session) return null;
+
+    return PrismaSessionMapper.toDomain(session);
   }
 }
