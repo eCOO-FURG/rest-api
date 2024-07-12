10561f3f4c9773b4b36387cf163a3027fe8b4b5b
timoteostifft <timoteostifft@gmail.com>
2024-07-11 23:44:01 -0300

    feat: mail password update type view type

--- src/core/mail/mailer.ts
+++ src/core/mail/mailer.ts
@@ -1,13 +1,20 @@
 // Entities
 import { Email } from "@/core/entities/email";
 
-export type MailerLoadRequest = {
-  view: "welcome";
-  props: {
-    first_name: string;
-    token: string;
-  };
-};
+export type MailerLoadRequest =
+  | {
+      view: "welcome";
+      props: {
+        first_name: string;
+        token: string;
+      };
+    }
+  | {
+      view: "password-update";
+      props: {
+        token: string;
+      };
+    };
 
 export interface Mailer {
   send(email: Email): Promise<void>;
