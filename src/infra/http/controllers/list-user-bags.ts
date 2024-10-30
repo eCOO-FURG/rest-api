5b309e2e0b491e665f1aad12f4d133fed3ae4841
timoteostifft <timoteostifft@gmail.com>
2024-10-30 00:55:56 -0300

    feat: list user bags controller filters

--- src/infra/http/controllers/list-user-bags.ts
+++ src/infra/http/controllers/list-user-bags.ts
@@ -17,10 +17,25 @@ import { toDate } from "@/infra/utils/to-date";
 // Validation
 import { realPeriod } from "@/infra/http/validation/real-period";
 
+// Utils
+import { toArray } from "@/infra/utils/to-array";
+
+// Entities
+import { Bag } from "@/core/entities/bag";
+
 export const listUserBagsSchema = {
   query: z
     .object({
       page: z.coerce.number().openapi({ description: "Página da listagem." }),
+      cycle_id: z
+        .string()
+        .uuid()
+        .optional()
+        .openapi({ description: "Ciclo da busca." }),
+      statuses: z
+        .string()
+        .optional()
+        .openapi({ description: "Filtro de status, separados por vírgula." }),
       since: z
         .string()
         .regex(/^\d{2}-\d{2}-\d{4}$/, "Formato esperado: DD-MM-YYYY")
@@ -44,15 +59,16 @@ export async function listUserBagsController(
   next: NextFunction
 ) {
   try {
-    const { page, since, before } = listUserBagsSchema.query.parse(
-      request.query
-    );
+    const { page, since, before, statuses, cycle_id } =
+      listUserBagsSchema.query.parse(request.query);
 
     const listBagsUseCase =
       container.resolve<ListBagsUseCase>("listBagsUseCase");
 
     const { bags } = await listBagsUseCase.execute({
       user_id: request.user_id,
+      cycle_id,
+      statuses: toArray<Bag["status"]>(statuses),
       since: toDate(since),
       before: toDate(before),
       page,
