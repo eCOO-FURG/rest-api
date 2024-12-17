87cff9d581bfc8de3e59e3775b3f1fa808f62713
timoteostifft <timoteostifft@gmail.com>
2024-12-17 02:10:03 -0300

    feat: sales stats use-case

---
+++ src/core/use-cases/fetch-sales-stats.ts
@@ -0,0 +1,45 @@
+// Repositories
+import { BagsRepository } from "@/core/repositories/bags-repository";
+
+export class FetchSalesStatsUseCase {
+  constructor(private bagsRepository: BagsRepository) {}
+
+  async execute() {
+    const today = new Date();
+
+    const FIVE_MONTHS_AGO = new Date(
+      today.getFullYear(),
+      today.getMonth() - 4,
+      1
+    );
+
+    const bags = await this.bagsRepository.list("merge", {
+      since: FIVE_MONTHS_AGO,
+    });
+
+    let revenue = 0;
+    const current = String(today.getMonth() + 1).padStart(2, "0");
+
+    const monthly = {} as Record<string, number>;
+    const daily = {} as Record<string, number>;
+
+    for (const bag of bags) {
+      const date = new Date(bag.created_at);
+
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+
+      if (!monthly[month]) monthly[month] = 0;
+
+      monthly[month] += bag.price();
+
+      if (!daily[day]) daily[day] = 0;
+
+      daily[day] += bag.price();
+
+      if (month === current) revenue += bag.price();
+    }
+
+    return { revenue, monthly, daily };
+  }
+}
