5e474258d11316554ed43cdf6426d62ac14b2069
LucasMachadoG <lucasgaelzermachado@gmail.com>
2025-01-01 14:32:15 -0300

    feat: Removal of phone and CPF validation

--- src/core/entities/phone.ts
+++ src/core/entities/phone.ts
@@ -2,15 +2,7 @@ export class Phone {
   private _value: string;
 
   constructor(value: string){
-    const numericPhone = value.replace(/\D/g, "");
-
-    if (!Phone.isValid(numericPhone)) return;
-
-    this._value = Phone.format(numericPhone);
-  }
-
-  static isValid(phone: string): boolean {
-    return /^[1-9]{2}9?[0-9]{8}$/.test(phone);
+    this._value = Phone.format(value);
   }
 
   static format(phone: string): string {
