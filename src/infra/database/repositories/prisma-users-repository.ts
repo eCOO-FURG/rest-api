519cb1b47324fd4e8315a971f419e5d102c73249
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-01-13 16:14:37 -0300

    fix: required property to use function findfirst, otherwise unable to authenticate

--- src/infra/database/repositories/prisma-users-repository.ts
+++ src/infra/database/repositories/prisma-users-repository.ts
@@ -22,7 +22,7 @@ export class PrismaUsersRepository implements UsersRepository {
     { id, email, cpf, phone, role }: UsersRepositorySearchRequest
   ): Promise<User | null> {
     const user = await prisma.user.findFirst({
-      where: { id, email, cpf, phone, roles: { has: role } },
+      where: { id, email, cpf, phone, roles: { has: role, isEmpty: false } },
     });
 
     if (!user) return null;
