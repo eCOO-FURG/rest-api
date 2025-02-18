f619c8d586b1de336c32ada70a43b49f1cfca66b
giovanni <br.giovanni@gmail.com>
2025-02-18 18:32:47 -0300

    feat: add chat user presenter and add chat in find user repository

--- src/infra/database/repositories/prisma-users-repository.ts
+++ src/infra/database/repositories/prisma-users-repository.ts
@@ -19,10 +19,10 @@ import { PrismaUserMapper } from "@/infra/database/mappers/prisma-user-mapper";
 export class PrismaUsersRepository implements UsersRepository {
   async find(
     _: RepositoryResponse,
-    { id, email, cpf, phone, role }: UsersRepositorySearchRequest
+    { id, email, cpf, phone, chat, role }: UsersRepositorySearchRequest
   ): Promise<User | null> {
     const user = await prisma.user.findFirst({
-      where: { id, email, cpf, phone, ...(role && { roles: { has: role } })},
+      where: { id, email, cpf, phone, chat, ...(role && { roles: { has: role } })},
     });
 
     if (!user) return null;
