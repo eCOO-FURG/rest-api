47d6e44fef7ad35bdd3f0d2a78081adb24f72d87
giovanni <br.giovanni@gmail.com>
2025-02-18 08:58:36 -0300

    feat: add param chat in update

--- src/core/use-cases/update-user.ts
+++ src/core/use-cases/update-user.ts
@@ -10,6 +10,8 @@ import { Storage } from "@/core/storage/storage";
 
 // Types
 import { File } from "@/core/types/file";
+import { CPF } from "../entities/cpf";
+import { Phone } from "../entities/phone";
 
 interface UpdateUserUseCaseRequest {
   user_id: string;
@@ -20,6 +22,7 @@ interface UpdateUserUseCaseRequest {
   phone?: string;
   password?: string;
   photo?: File;
+  chat?: string;
 }
 
 export class UpdateUserUseCase {
@@ -38,6 +41,7 @@ export class UpdateUserUseCase {
     phone,
     password,
     photo,
+    chat
   }: UpdateUserUseCaseRequest) {
     const user = await this.usersRepository.find("basic", { id: user_id });
 
@@ -46,8 +50,9 @@ export class UpdateUserUseCase {
     user.first_name = first_name ?? user.first_name;
     user.last_name = last_name ?? user.last_name;
     user.email = email ?? user.email;
-    user.cpf = cpf ?? user.cpf;
-    user.phone = phone ?? user.phone;
+    user.cpf = new CPF(cpf ?? user.cpf.value);
+    user.phone = new Phone(phone ?? user.phone.value);
+    user.chat = chat ?? user.chat;
 
     if (password) user.password = await this.encrypter.encrypt(password);
 
