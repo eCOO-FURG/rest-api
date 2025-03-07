92f10b0750401f66c2a5974922281130266fc040
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-07 11:28:15 -0300

    refactor(spreadsheet-service): add more props to `sales-report` type, and create `sales-producers`, `sales-products`, and `sales-consumers` new spreadsheet types

--- src/core/report/spreadsheet-service.ts
+++ src/core/report/spreadsheet-service.ts
@@ -1,5 +1,6 @@
 // Entities
 import { Bag } from "@/core/entities/bag";
+import { Catalog } from "@/core/entities/catalog";
 
 // Types
 import { File } from "@/core/types/file";
@@ -8,12 +9,31 @@ export interface SpreadsheetColumn {
   header: string;
   key: string;
   width?: number;
+  style?: { numFmt: string };
 }
 
-export type SpreadsheetServiceGenerateRequest = {
-  type: "sales-report";
-  props: { bags: Bag[] };
-};
+export type SpreadsheetServiceGenerateRequest =
+  | {
+      type: "sales-report";
+      props: {
+        start_date?: Date;
+        end_date?: Date;
+        bags: Bag[];
+        catalogs: Catalog[];
+      };
+    }
+  | {
+      type: "sales-products";
+      props: { start_date?: Date; end_date?: Date; bags: Bag[] };
+    }
+  | {
+      type: "sales-producers";
+      props: { start_date?: Date; end_date?: Date; catalogs: Catalog[] };
+    }
+  | {
+      type: "sales-consumers";
+      props: { start_date?: Date; end_date?: Date; bags: Bag[] };
+    };
 
 export interface SpreadsheetService {
   generate({ type, props }: SpreadsheetServiceGenerateRequest): Promise<File>;
