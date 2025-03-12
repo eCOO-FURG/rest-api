bf3587b9ab50a6ef94b560e3748d3d1d9086cd00
timoteostifft <timoteostifft@gmail.com>
2025-03-12 13:51:20 -0300

    fix: box routes

--- src/infra/http/routes/boxes.ts
+++ src/infra/http/routes/boxes.ts
@@ -12,5 +12,5 @@ import { fetchCurrentBoxController } from "@/infra/http/controllers/fetch-curren
 export const boxes = Router();
 
 boxes.get("/", ensureRole(["BROKER"]), listBoxesController);
-boxes.get("/:box_id", ensureRole(["BROKER"]), fetchBoxController);
 boxes.get("/current", ensureRole(["PRODUCER"]), fetchCurrentBoxController);
+boxes.get("/:box_id", ensureRole(["BROKER"]), fetchBoxController);
