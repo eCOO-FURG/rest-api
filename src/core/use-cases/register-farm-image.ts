1d31e443bb1e520df151234975403feb6afad597
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-15 09:44:36 -0300

    fix: add error handling for unauthorized access and inactive farms in register farm image use case

--- src/core/use-cases/register-farm-image.ts
+++ src/core/use-cases/register-farm-image.ts
@@ -3,8 +3,10 @@ import { FarmsRepository } from "@/core/repositories/farms-repository";
 import { UsersRepository } from "@/core/repositories/users-repository";
 
 // Errors
+import { FarmNotActiveError } from "@/core/errors/farm-not-active";
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found";
 import { ResourceReachedLimitError } from "@/core/errors/resource-reached-limit";
+import { UnauthorizedError } from "@/core/errors/unauthorized";
 
 // Services
 import { Storage } from "@/core/storage/storage";
@@ -35,7 +37,9 @@ export class RegisterFarmImageUseCase {
     if (!user) throw new ResourceNotFoundError("Usuário", user_id);
 
     if (!farm.admin_id.equals(user.id) && !user.admin)
-      throw new ResourceNotFoundError("Fazenda", farm_id);
+      throw new UnauthorizedError();
+
+    if (farm.status !== "ACTIVE") throw new FarmNotActiveError();
 
     if (farm.images.size >= 4)
       throw new ResourceReachedLimitError("Fazenda", farm_id, "images");
