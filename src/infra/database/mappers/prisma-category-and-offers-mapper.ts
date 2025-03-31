6352bc314d3f8e51d8c540a5cf1013051f2e4a21
timoteostifft <timoteostifft@gmail.com>
2025-03-31 13:13:09 -0300

    feat: category and offers infra aggregate

---
+++ src/infra/database/mappers/prisma-category-and-offers-mapper.ts
@@ -0,0 +1,49 @@
+// Entities
+import { UUID } from "@/core/entities/aggregates/uuid";
+import { CategoryAndOffers } from "@/core/entities/aggregates/category-and-offers";
+
+// Libraries
+import {
+  Category as PrismaCategory,
+  Product as PrismaProduct,
+} from "@prisma/client";
+
+// Mappers
+import {
+  PrismaOfferAndDetails,
+  PrismaOfferAndDetailsMapper,
+} from "@/infra/database/mappers/prisma-offer-and-details-mapper";
+
+// Repositories
+import {
+  CategoryRepositoryReturnType,
+  CategoryEntityOf,
+} from "@/core/repositories/categories-repository";
+
+export type PrismaCategoryAndOffers = PrismaCategory & {
+  products: PrismaProduct &
+    {
+      offers: PrismaOfferAndDetails[];
+    }[];
+};
+
+export class PrismaCategoryAndOffersMapper {
+  static toDomain<
+    T extends CategoryRepositoryReturnType = "category-and-offers"
+  >(raw: PrismaCategoryAndOffers): CategoryEntityOf<T> {
+    return CategoryAndOffers.create({
+      id: new UUID(raw.id),
+      name: raw.name,
+      created_at: raw.created_at,
+      updated_at: raw.updated_at,
+      offers: raw.products.flatMap((product) =>
+        product.offers.map((offer) =>
+          PrismaOfferAndDetailsMapper.toDomain({
+            ...offer,
+            product: { ...offer.product },
+          })
+        )
+      ),
+    }) as CategoryEntityOf<T>;
+  }
+}
