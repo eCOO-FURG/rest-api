4e1be4b07a3098faacdc456abf06f67a9f0a01db
timoteostifft <timoteostifft@gmail.com>
2025-04-05 22:27:58 -0300

    feat: prisma order and details

--- src/core/entities/aggregates/offer-and-product.ts
+++ src/core/entities/aggregates/offer-and-product.ts
@@ -2,9 +2,6 @@
 import { Offer, OfferProps } from "@/core/entities/offer";
 import { Product } from "@/core/entities/product";
 
-// Types
-import { Optional } from "@/core/types/optional";
-
 export interface OfferAndProductProps extends OfferProps {
   product: Product;
 }
