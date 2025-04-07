d2371af65379837259557e8d8f77fe3167481ed8
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-04-07 10:19:28 -0300

    feat: add optional `statuses` parameter to `FetchBagUseCaseRequest` and update `execute` method to handle it

--- src/core/use-cases/fetch-bag.ts
+++ src/core/use-cases/fetch-bag.ts
@@ -2,6 +2,9 @@
 import { BagsRepository } from "@/core/repositories/bags-repository";
 import { UsersRepository } from "@/core/repositories/users-repository";
 
+// Entities
+import { OrderStatus } from "@/core/entities/order";
+
 // Errors
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found";
 
@@ -9,6 +12,7 @@ interface FetchBagUseCaseRequest {
   user_id: string;
   bag_id: string;
   page: number;
+  statuses?: OrderStatus[];
 }
 
 export class FetchBagUseCase {
@@ -17,10 +21,16 @@ export class FetchBagUseCase {
     private usersRepository: UsersRepository
   ) {}
 
-  async execute({ bag_id, user_id, page }: FetchBagUseCaseRequest) {
+  async execute({ bag_id, user_id, page, statuses }: FetchBagUseCaseRequest) {
+    console.log("Fetching bag...");
+    console.log("Bag ID:", bag_id);
+    console.log("User ID:", user_id);
+    console.log("Page:", page);
+    console.log("Statuses:", statuses);
     const bag = await this.bagsRepository.find("bag-and-orders", {
       id: bag_id,
       orders: { page },
+      orderStatuses: statuses,
     });
 
     if (!bag) throw new ResourceNotFoundError("Sacola", bag_id);
