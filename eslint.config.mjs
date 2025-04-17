559aade89182958aaf561674dca350ca775cfe44
timoteostifft <timoteostifft@gmail.com>
2025-04-17 13:35:28 -0300

    fix: eslint vitest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-import vitest from "vitest";
+import * as vitest from "vitest";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
