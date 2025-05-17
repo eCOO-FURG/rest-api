12fb0c6aedba0a228629521cf879e9a8009a1c87
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-05-17 14:00:34 -0300

    refactor: enforce `BROKER` role on bag patch route

--- src/infra/http/routes/bags.ts
+++ src/infra/http/routes/bags.ts
@@ -6,9 +6,9 @@ import { ensureAuthenticated } from "@/infra/http/middlewares/ensure-authenticat
 import { ensureRole } from "@/infra/http/middlewares/ensure-role";
 
 // Controllers
-import { listCurrentBagsController } from "@/infra/http/controllers/list-current-bags";
-import { listBagsController } from "@/infra/http/controllers/list-bags";
 import { fetchBagController } from "@/infra/http/controllers/fetch-bag";
+import { listBagsController } from "@/infra/http/controllers/list-bags";
+import { listCurrentBagsController } from "@/infra/http/controllers/list-current-bags";
 import { updateBagController } from "@/infra/http/controllers/update-bag";
 
 export const bags = Router();
@@ -17,4 +17,4 @@ bags.get("/", listBagsController);
 bags.get("/current", ensureRole(["BROKER"]), listCurrentBagsController);
 bags.get("/:bag_id", fetchBagController);
 
-bags.patch("/:bag_id", ensureAuthenticated, updateBagController);
+bags.patch("/:bag_id", ensureRole(["BROKER"]), ensureAuthenticated, updateBagController);
