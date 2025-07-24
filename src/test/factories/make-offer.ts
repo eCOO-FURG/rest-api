e44c0f290dc0f605fd58076d7b71004cbe51c059
timoteostifft <timoteostifft@gmail.com>
2025-07-24 13:45:40 -0300

    fix: offer factory

--- src/test/factories/make-offer.ts
+++ src/test/factories/make-offer.ts
@@ -10,6 +10,8 @@ export function makeOffer(props: Partial<OfferProps> = {}) {
     ...props,
     product_id: props.product_id ?? new UUID(),
     catalog_id: props.catalog_id ?? new UUID(),
+    opens_at: props.opens_at ?? faker.date.past(),
+    closes_at: props.closes_at ?? faker.date.future(),
     amount: props.amount ?? faker.number.int({ min: 10, max: 12 }),
     price: props.price ?? faker.number.int({ min: 5, max: 50 }),
     fee: props.fee ?? faker.number.int({ min: 1, max: 10 }),
