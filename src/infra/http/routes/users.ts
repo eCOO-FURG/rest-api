350e0ab5bcca03326878f507d09b3d7438415bd3
timoteostifft <timoteostifft@gmail.com>
2025-09-16 16:32:37 -0300

    feat: ensure auth on list users

--- src/infra/http/routes/users.ts
+++ src/infra/http/routes/users.ts
@@ -5,7 +5,10 @@ import { Router } from "express";
 import { registerController } from "@/infra/http/controllers/register";
 import { listUsersController } from "@/infra/http/controllers/list-users";
 
+// Middleware
+import { ensureAuthenticated } from "@/infra/http/middlewares/ensure-authenticated";
+
 export const users = Router();
 
 users.post("/", registerController);
-users.get("/", listUsersController);
+users.get("/", ensureAuthenticated, listUsersController);
